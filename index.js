9fdc6bb81eea9b6859b5c71bf8526422a7860f82
mamad-alaee <mamadalaee@gmail.com>
2025-04-09 19:03:59 +0330

    javabe soala haye ajib qarib

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ video_getter_btn.addEventListener("click",function (){
 
 function click_handler(){
     document.getElementById("video_h1").innerHTML = "show video"
+    document.getElementById("mainBody").style.backgroundColor = ""
 }
 
 
+// document.getElementById("name_inputio").addEventListener("keydown",function (){
+//     console.log("mouse enter")
+// })
+
+
+function mamad_heydarie(){
+    let colors = ["red","green","blue","yellow"]
+    rand_index = Math.floor(Math.random() * colors.length)
+   document.getElementById("name_inputio").style.backgroundColor = colors[rand_index]
+}
+
+
+function def_color(){
+    document.getElementById("name_inputio").style.backgroundColor = "white"
+
+}
