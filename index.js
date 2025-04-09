d09ef1dad68a62f203cc0bb11abadfb587e36693
mamad-alaee <mamadalaee@gmail.com>
2025-04-09 18:37:43 +0330

    init base files

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+const video_getter_btn = document.getElementById("video_getter_btn")
+video_getter_btn.addEventListener("click",function (){
+    document.getElementById("video_h1").innerHTML = "show video"
+})
+
+
+
+
+function click_handler(){
+    document.getElementById("video_h1").innerHTML = "show video"
+}
+
+
