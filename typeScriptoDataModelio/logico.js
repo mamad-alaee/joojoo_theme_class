980961c7a8f004c6f237afca781227c61810805b
mamad-alaee <mamadalaee@gmail.com>
2025-05-14 19:13:48 +0330

    type in typeScript

---
+++ typeScriptoDataModelio/logico.js
@@ -0,0 +1,37 @@
+
+// every type of error
+// 1. Syntax Error
+// 2.compile time error
+// 3. runtime error
+// 4. logical error
+
+
+
+let age=23
+age = "25"
+
+
+
+let userName = "John Doe"
+userName = 24
+
+
+let isMarried = true
+isMarried = "Yes"
+
+
+//dictatorrrrrrrrrrrrrrrrrrrrrrrrrrr
+
+
+let userList = ["bardia","bardiaWithSh","bardiaWithSh2"]
+
+
+
+
+
+
+
+
+
+
+
---
+++ typeScriptoDataModelio/lolo.ts
@@ -0,0 +1,32 @@
+
+
+
+let age2:number=23;
+age = "25"
+
+
+
+let username:string = "John Doe";
+username = 24;
+
+
+
+let isLoggedIn:boolean = true;
+isLoggedIn = "false";
+
+
+
+let studentList:string[] = ["bardia","bardiaWithSh","bardiaWithSh2"]
+studentList.push(23)
+
+
+
+let bardiaData:{name:string,skill:string,game:string,aka:string,age:number} = {name:"bardia",skill:"ش",game:"war thunder",aka:"timePoor",age:15}
+bardiaData.name=23
+
+
+let behdadData:[string,number,string,string,boolean] = ["behdad",15,"valorant","noob",false]
+behdadData[3] = "Normal"
+behdadData[0] = 12
+
+
